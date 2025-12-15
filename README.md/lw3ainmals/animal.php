7837357d36f3a87a22b29e6036abc60f65ee795e
kirillzhuravlev09200 <lektordark2017@gmail.com>
2025-12-15 23:38:56 +0300

    Create animal.php

---
+++ README.md/lw3ainmals/animal.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Animals;
+
+abstract class Animal
+{
+    private string $name;
+    private int $age;
+    private string $species;
+
+    public function __construct(string $name, int $age, string $species) {
+    public function __construct(string $name, int $age, string $species)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->species = $species;
+    }
+
+    public function getName(): string {
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getAge(): int {
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    public function getSpecies(): string {
+    public function getSpecies(): string
+    {
+        return $this->species;
+    }
+
+    abstract public function makeSound(): void;
+}
