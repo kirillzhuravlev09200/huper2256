1d3182daed15a12f4aaf0721884885c9a5b92b2a
kirillzhuravlev09200 <lektordark2017@gmail.com>
2025-12-16 00:05:27 +0300

    Formatting PHP files according to standards, using autoloading

---
+++ README.md/lw7/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+spl_autoload_register(static function (string $class): void {
+    $prefix = 'App\\';
+    $baseDir = __DIR__ . DIRECTORY_SEPARATOR;
+
+    if (str_starts_with($class, $prefix)) {
+        $relativeClass = substr($class, strlen($prefix));
+        $file = $baseDir . str_replace('\\', DIRECTORY_SEPARATOR, $relativeClass) . '.php';
+
+        if (is_file($file)) {
+            require $file;
+        }
+    }
+});
