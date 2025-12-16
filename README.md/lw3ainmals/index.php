bf03116c891e9f367921b714d6703979d3d9a996
kirillzhuravlev09200 <lektordark2017@gmail.com>
2025-12-16 14:17:51 +0300

    Main Commit build

--- README.md/lw3ainmals/index.php
+++ README.md/lw3ainmals/index.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 require __DIR__ . '/autoload.php';
 
-use Animals\Cat;
-use Animals\Dog;
-use Animals\Zoo;
+use Animals\cat;
+use Animals\dog;
+use Animals\zoo;
 
 $zoo = new Zoo();
-$zoo->addAnimal(new Dog('Барбос', 4, 'Терьер'));
-$zoo->addAnimal(new Cat('Мурка', 2, 'Черный'));
-$zoo->addAnimal(new Dog('Рекс', 6, 'Овчарка'));
+$zoo->addAnimal(new dog('Барбос', 4, 'Терьер'));
+$zoo->addAnimal(new cat('Мурка', 2, 'Черный'));
+$zoo->addAnimal(new dog('Рекс', 6, 'Овчарка'));
 
 $zoo->listAnimals();
 $zoo->animalSounds();
