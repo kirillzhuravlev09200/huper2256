160742c7bd8270b396dcc9fea9ecafe8373f2548
kirillzhuravlev09200 <lektordark2017@gmail.com>
2025-12-16 18:44:24 +0300

    first commit

---
+++ README.md/lw3/Zoo.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Animals;
+
+class Zoo
+{
+    private array $animals = [];
+
+    public function addAnimal(Animal $animal): void
+    {
+        $this->animals[] = $animal;
+    }
+
+    public function listAnimals(): void
+    {
+        echo 'Животные в зоопарке:' . '<br>';
+        foreach ($this->animals as $animal) {
+            echo sprintf(
+                '- %s (%s), возраст %d' . '<br>',
+                $animal->getName(),
+                $animal->getSpecies(),
+                $animal->getAge()
+            );
+        }
+    }
+
+    public function animalSounds(): void
+    {
+        echo '<br>' . 'Звуки животных:' . '<br>';
+        foreach ($this->animals as $animal) {
+            $animal->makeSound();
+        }
+    }
+}
