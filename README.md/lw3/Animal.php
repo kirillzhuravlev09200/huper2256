17a52d1dcf04781b95200c2dfeb7fa90cff23d4d
kirillzhuravlev09200 <lektordark2017@gmail.com>
2025-12-16 18:41:01 +0300

    Update Animal.php

--- README.md/lw3/Animal.php
+++ README.md/lw3/Animal.php
@@ -1,1 +1,36 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Animals;
+
+abstract class Animal
+{
+    private string $name;
+    private int $age;
+    private string $species;
+
+    public function __construct(string $name, int $age, string $species)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->species = $species;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    public function getSpecies(): string
+    {
+        return $this->species;
+    }
+
+    abstract public function makeSound(): void;
+}
