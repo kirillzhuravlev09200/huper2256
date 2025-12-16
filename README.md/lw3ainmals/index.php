1ae931460de07645297bcff6a752805186c96ec2
kirillzhuravlev09200 <lektordark2017@gmail.com>
2025-12-16 14:18:13 +0300

    Main Commit build

--- README.md/lw3ainmals/index.php
+++ README.md/lw3ainmals/index.php
@@ -8,7 +8,7 @@ use Animals\cat;
 use Animals\dog;
 use Animals\zoo;
 
-$zoo = new Zoo();
+$zoo = new zoo();
 $zoo->addAnimal(new dog('Барбос', 4, 'Терьер'));
 $zoo->addAnimal(new cat('Мурка', 2, 'Черный'));
 $zoo->addAnimal(new dog('Рекс', 6, 'Овчарка'));
